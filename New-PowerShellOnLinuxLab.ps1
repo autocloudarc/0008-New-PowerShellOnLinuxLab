73528624b227ce5efd0ea95456b33e6142bf0abf
Preston K. Parsard <autocloudarc@outlook.com>
2017-11-04 22:26:23 -0400

    Added copyright notice, conditions and disclaimers for Posh-SSH.

--- New-PowerShellOnLinuxLab.ps1
+++ New-PowerShellOnLinuxLab.ps1
@@ -88,6 +88,21 @@
     (iii) to indemnify, hold harmless, and defend Us and Our suppliers from and against any claims or lawsuits, including attorneys' fees, that arise or result from the use or distribution of the Sample Code.
     This posting is provided "AS IS" with no warranties, and confers no rights.
 
+    Posh-SSH
+    Copyright (c) 2015, Carlos Perez All rights reserved.
+    Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * Neither the name of Posh-SSH nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED 
+    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+    PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+    HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+    POSSIBILITY OF SUCH DAMAGE.
+
 .COMPONENT
     Azure IaaS
 .ROLE
@@ -162,7 +177,7 @@ function Get-PSGalleryModule
             Uninstall-Module -Name $Module -AllVersions -Force -ErrorAction SilentlyContinue -Verbose
 		} #end
 
-		# If the WriteToLogs module isn't already loaded, install and import it for use later in the script for logging operations
+		# If the modules aren't already loaded, install and import it
 		If (!(Get-Module -Name $Module))
 		{
 			# https://www.powershellgallery.com/packages/WriteToLogs
